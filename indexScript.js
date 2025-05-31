a617d78f75df20f8a17961f311cee09b7f59e87e
Poorna <poornarajapaksha339@gamil.com>
2025-05-31 18:27:26 +0530

    Add video resources

---
+++ indexScript.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const btnBook = document.querySelector(".btn btn-primary");
+    const btnLog = document.querySelector(".btn btn-outline");
+
+    btnBook.addEventListener("click",function(){
+        window.location.href="";
+        // TODO : Specify the link
+    });
+
+    btnLog.addEventListener("click",function(){
+        window.location.href="frontend/html/auth/login.html";
+    });
+});
